952cc1c222a5a238aaa8ab9e5306ac8c7e59594a
devsachin0879 <38144864+devsachin0879@users.noreply.github.com>
2022-02-17 23:39:16 +0200

    Add files via upload

---
+++ myProgram.py
@@ -0,0 +1,7 @@
+# want to use code from myModulr file
+
+from myMainPackage import some_main_script
+from myMainPackage.SubPackage import mySubScript
+
+some_main_script.report_main()
+mySubScript.sub_report()
